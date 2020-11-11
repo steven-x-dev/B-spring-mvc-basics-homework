45b6f36fbee72cfbbb84344bebd356750443cd42
steven-x-dev <steven-dev@qq.com>
2020-11-11 12:14:00 +0800

    [steven] code: removed unused imports

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/controller/UserController.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/controller/UserController.java
@@ -3,7 +3,6 @@ package com.thoughtworks.capacity.gtb.mvc.controller;
 import com.thoughtworks.capacity.gtb.mvc.domain.User;
 import com.thoughtworks.capacity.gtb.mvc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
